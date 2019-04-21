c1d9aa17226340f15ad9b4a7f7acd655ab721126
Ohene Bonsu <oheneb.bonsu@gmail.com>
2019-04-20 19:58:05 -0500

    Class Properties set

---
+++ getUser/user.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace getUser
+{
+   
+}
+class User
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FaveAnimal { get; set; }
+    public string FaveSport { get; set; }
+    public string FaveNumber { get; set; }
+}
