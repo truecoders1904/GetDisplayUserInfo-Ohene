b556c4cadad4743c9a3573c0733d694260e0e128
Ohene Bonsu <oheneb.bonsu@gmail.com>
2019-04-23 07:56:19 -0500

    Touch ups

--- getUser/user.cs
+++ getUser/user.cs
@@ -2,28 +2,25 @@
 
 namespace getUserInfo
 {
-   
-}
-class User
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string FaveAnimal { get; set; }
-    public string FaveSport { get; set; }
-    public int FaveNumber { get; set; }
-
-//EMPY FIRST CW LINE,  TEST
-    public string AnswerQuestion(string question)
+    class User
     {
-        Console.WriteLine(question);
-        return Console.ReadLine();
-    }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FaveAnimal { get; set; }
+        public string FaveSport { get; set; }
+        public int FaveNumber { get; set; }
+        public string AnswerQuestion(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
 
-    public int AnswerQuestionWithInteger(string question)
-    {
-        string stringAnswer = AnswerQuestion(question);
-        return int.Parse(stringAnswer);
+        public int AnswerQuestionWithInteger(string question)
+        {
+            string stringAnswer = AnswerQuestion(question);
+            return int.Parse(stringAnswer);
+        }
+
     }
 
 }
-
